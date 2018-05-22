a38a28b8c25f4936278efe87a60455d168230fda
Dwight Gunning <dwight@dwightgunning.com>
2018-05-22 18:48:47 +0200

    Run linters on Travis build w/ latest node version

--- bin/travis.sh
+++ bin/travis.sh
@@ -3,9 +3,9 @@ set -e
 ./node_modules/.bin/webpack
 npm test
 ./bin/coveralls.sh
-if [ "$(node --version)" == "v6.7.0" ]; then
+if [ ${RUN_LINTERS:="no"} == "yes" ]; then
     ./node_modules/.bin/eslint src/**/*.js;
-    ./node_modules/.bin/dtslint types/getstream
+    # ./node_modules/.bin/dtslint types/getstream
 fi
 npm install -g bower
 bower install getstream
